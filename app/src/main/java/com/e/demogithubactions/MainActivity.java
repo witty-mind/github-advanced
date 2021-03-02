691cdf375be665ba26348c4e29b18dfd688048ff
Debasish <dark_vanilla_00@yahoo.com>
2021-03-02 19:13:15 +0530

    Added Data Add to the MainScreen.

--- app/src/main/java/com/e/demogithubactions/MainActivity.java
+++ app/src/main/java/com/e/demogithubactions/MainActivity.java
@@ -1,7 +1,11 @@
 package com.e.demogithubactions;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addData();
+    }
+
+    // Function responsible for adding data to the array
+    public void addData() {
+        try {
+            ArrayList<String> stringArray = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                stringArray.add("This is" + i);
+            }
+            Toast.makeText(this, "Data added to array successfully", Toast.LENGTH_SHORT).show();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
