b2bab16e1af34bd1d87ca1e6461df497318c9808
Debasish <dark_vanilla_00@yahoo.com>
2021-03-03 12:30:35 +0530

    Removed the Class from the main function

--- app/src/main/java/com/e/demogithubactions/MainActivity.java
+++ app/src/main/java/com/e/demogithubactions/MainActivity.java
@@ -70,32 +70,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
     }
 
-    public static class Demo {
-        double my_real;
-        double my_imag;
-
-        public Demo(double my_real, double my_imag) {
-            this.my_real = my_real;
-            this.my_imag = my_imag;
-        }
-
-        public static void main(String[] args) {
-            Demo n1 = new Demo(76.8, 24.0),
-                    n2 = new Demo(65.9, 11.23),
-                    temp;
-            temp = add(n1, n2);
-            System.out.printf("The sum of two complex numbers is %.1f + %.1fi", temp.my_real,
-                    temp.my_imag);
-        }
-
-        public static Demo add(Demo n1, Demo n2) {
-            Demo temp = new Demo(0.0, 0.0);
-            temp.my_real = n1.my_real + n2.my_real;
-            temp.my_imag = n1.my_imag + n2.my_imag;
-            Log.d("This is done", "Yes");
-        }
-    }
-
     // Function responsible for checking the number is positive or negative
     public void checkNumber() {
         double number = 12.3;
