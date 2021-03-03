58148575fdc4e928c742ccef84ec9d077bf94ab2
Debasish Kumar Das <debasishkumardas5@gmail.com>
2021-03-03 12:23:40 +0530

    Log Added (#8)
    * Log Added
    
    * Added Prime Number check.
    
    Co-authored-by: Debasish <dark_vanilla_00@yahoo.com>

--- app/src/main/java/com/e/demogithubactions/MainActivity.java
+++ app/src/main/java/com/e/demogithubactions/MainActivity.java
@@ -89,7 +89,26 @@ public class MainActivity extends AppCompatActivity {
             Demo temp = new Demo(0.0, 0.0);
             temp.my_real = n1.my_real + n2.my_real;
             temp.my_imag = n1.my_imag + n2.my_imag;
+            Log.d("This is done", "Yes");
             return(temp);
         }
+    }
+
+    // Function to check prime numbers
+    public void checkPrimeNumber(){
+        int num = 29;
+        boolean flag = false;
+        for (int i = 2; i <= num / 2; ++i) {
+            // condition for nonprime number
+            if (num % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
     }
 }
